12d651764368420998a28469b7b1884f9f30aa5f
Evan Seeyave <evan.seeyave@gmail.com>
2024-09-19 11:42:02 -0400

    Set up firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATGwC0IlPagtYw-7OVicGdUF_dhnmuoxc",
+  authDomain: "ai-ui-generator.firebaseapp.com",
+  projectId: "ai-ui-generator",
+  storageBucket: "ai-ui-generator.appspot.com",
+  messagingSenderId: "834430302725",
+  appId: "1:834430302725:web:28506ee44aa9d3c4ebfa6c",
+  measurementId: "G-YXXCTGMZER"
+};
+
+export default firebaseConfig;
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import firebaseConfig from './firebaseConfig'
 
 import App from './App'
 import { persistor, store } from './redux/store'
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
